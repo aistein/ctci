93b9172d128b0205083cd08a1fb082a4f88ae42b
Alexander Stein <alex@alexstein.dev>
2021-03-16 09:40:20 -0500

    adds in-place quicksort implementation of ch1-p1 isUnique

--- ch1/problem-1.1-is-unique.go
+++ ch1/problem-1.1-is-unique.go
@@ -1,5 +1,10 @@
 package ch1
 
+import (
+	"math/rand"
+	"time"
+)
+
 type MutableString []rune
 type IsUnique func(s *MutableString) bool
 
@@ -23,4 +28,78 @@ func isuniqueHashmap(s *MutableString) bool {
 		seen[runeVal] = true
 	}
 	return true
+}
+
+func isuniqueQuicksort(s *MutableString) bool {
+	if len(*s) < 2 {
+		return true
+	}
+
+	quicksortMutableString(0, len(*s), s)
+
+	prevRune := (*s)[0]
+	for i := 1; i < len(*s); i++ {
+		if (*s)[i] == prevRune {
+			return false
+		}
+		prevRune = (*s)[i]
+	}
+	return true
+}
+
+func quicksortMutableString(l, h int, s *MutableString) {
+	if l < h {
+		j := pivotMutableString(l, h, s, func(l, h int) int {
+			return l +rand.Intn(h - l)
+		})
+		quicksortMutableString(l, j, s)
+		quicksortMutableString(j+1, h, s)
+	}
+}
+
+// pivotIndexFunction must return an index in the range [l, h)
+type pivotIndexFunction func(l, h int) int
+
+// pivotMutableString selects a pivot from s using the supplied pivotIndexFunction; then it reorganizes s in place so
+// that everything left of the pivot is less than the pivot, and everything after the pivot is greater than or equal to
+// the pivot.  If s contains only unique values, the index where the pivot lands after reorganization is returned.  If s
+// contains duplicate pivot values, the index where the rightmost duplicate of the pivot lands is returned.
+func pivotMutableString(l, h int, s *MutableString, getIndex pivotIndexFunction) int {
+	if h - l == 1 {
+		return l
+	}
+
+	if h - l == 2 {
+		if (*s)[l] > (*s)[l+1] {
+			(*s)[l], (*s)[l+1] = (*s)[l+1], (*s)[l]
+		}
+		return l+1
+	}
+
+	pivotIdx := getIndex(l, h)
+	pivot := (*s)[pivotIdx]
+
+	lt, current, gt := l, l, h-1
+	for current <= gt {
+		if (*s)[current] < pivot {
+			(*s)[current], (*s)[lt] = (*s)[lt], (*s)[current]
+			current, lt = current+1, lt+1
+			continue
+		}
+		if (*s)[current] == pivot {
+			current++
+			continue
+		}
+		if  (*s)[current] > pivot {
+			(*s)[current], (*s)[gt] = (*s)[gt], (*s)[current]
+			gt--
+			continue
+		}
+	}
+	return current - 1
+}
+
+func init() {
+	// Quicksort will randomly select a pivot, so we want to intelligently seed the random number generator
+	rand.Seed(time.Now().UnixNano())
 }
--- ch1/problem-1.1-is-unique_test.go
+++ ch1/problem-1.1-is-unique_test.go
@@ -19,16 +19,68 @@ func TestIsUnique(t *testing.T) {
 		{name: "Mixed Long Unique", input: "aBcdefghijkLmnopqRstuvWxyz@!+=#2^", expected: true},
 		{name: "Mixed Long Not Unique", input: "saiouaFASKDFjasdfhkli$^$%E(+++", expected: false},
 	}
-	implementations := []IsUnique{
-		isuniqueBruteforce,
-		isuniqueHashmap,
+	implementations := []struct {
+		name string
+		fn IsUnique
+	}{
+		{name: "Brute Force", fn: isuniqueBruteforce},
+		{name: "Hash Map", fn: isuniqueHashmap},
+		{name: "Quicksort", fn: isuniqueQuicksort},
 	}
 	for _, implementation := range implementations {
 		for _, tc := range tests {
-			t.Run(tc.name, func(t *testing.T){
+			t.Run(implementation.name + "/" + tc.name, func(t *testing.T){
 				str := MutableString(tc.input)
-				require.Equal(t, tc.expected, implementation(&str))
+				require.Equal(t, tc.expected, implementation.fn(&str))
 			})
 		}
+	}
+}
+
+func TestPivotMutableString(t *testing.T) {
+	tests := []struct {
+		name string
+		input string
+		expected int
+	}{
+		{name: "Singleton", input: "a", expected: 0},
+		{name: "Double Unique", input: "ab", expected: 1},
+		{name: "Double Not Unique", input: "aa", expected: 1},
+		{name: "Short Unique", input: "dbcaefg", expected: 3},
+		{name: "Short Not Unique", input: "dbcadfg", expected: 4},
+		{name: "Long Unique", input: "xyzwabqfglnmpr", expected: 11},
+		{name: "Long Not Unique", input: "xyzwabxqfglnmpfr", expected: 13},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ms := MutableString(tc.input)
+			getStablePivotIndex := func(l, h int) int {
+				return l
+			}
+			require.Equal(t, tc.expected, pivotMutableString(0, len(ms), &ms, getStablePivotIndex))
+		})
+	}
+}
+
+func TestQuicksortMutableString(t *testing.T) {
+	tests := []struct {
+		name string
+		input string
+		expected string
+	}{
+		{name: "Singleton", input: "a", expected: "a"},
+		{name: "Double Unique", input: "ba", expected: "ab"},
+		{name: "Double Not Unique", input: "aa", expected: "aa"},
+		{name: "Short Unique", input: "dbcaefg", expected: "abcdefg"},
+		{name: "Short Not Unique", input: "dbcadfg", expected: "abcddfg"},
+		{name: "Long Unique", input: "xyzwabqfglnmpr", expected: "abfglmnpqrwxyz"},
+		{name: "Long Not Unique", input: "xyzwabqfglnmpfr", expected: "abffglmnpqrwxyz"},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ms := MutableString(tc.input)
+			quicksortMutableString(0, len(ms), &ms)
+			require.Equal(t, tc.expected, string(ms))
+		})
 	}
 }
